c80d2520db930ad4dee2121001229f59bbf603a4
Colldub <Worthcollin@gmail.com>
2023-10-18 09:55:41 -0700

    First Commit

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+/*
+    Calculator Assignment CS 121
+    Main file
+
+    Collin Worth 10-15-2023
+
+*/
+#include "Stack.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    Stack s;
+    char operators;
+
+    do{
+        s.exit = false;
+
+        s.PushAndSend(s);
+        s.DisplayTop();
+
+    }while(s.exit == false);
+
+    s.Delete();
+    return 0;
+}
